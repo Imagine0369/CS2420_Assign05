b3b72cf7a025e5097fa734e7e12aa547be506680
Imagine0369 <77407532+Imagine0369@users.noreply.github.com>
2021-10-07 14:32:28 -0600

    Added QuickSort and MergeSort tests

--- assignment5/src/assign05/ArrayListSorterTest.java
+++ assignment5/src/assign05/ArrayListSorterTest.java
@@ -66,42 +66,56 @@ class ArrayListSorterTest {
 	void TestOneMergeIntegers() {
 		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(1);
 		ArrayListSorter.mergesort(arr);
-		assertEquals(arr, ArrayListSorter.generateAscending(1));
+		assertEquals(ArrayListSorter.generateAscending(1), arr);
 	}
 	
 	@Test
 	void TestTwoMergeIntegers() {
 		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(2);
 		ArrayListSorter.mergesort(arr);
-		assertEquals(arr, ArrayListSorter.generateAscending(2));
+		assertEquals(ArrayListSorter.generateAscending(2), arr);
 	}
 	
 	@Test
 	void TestThreeMergeIntegers() {
 		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(3);
 		ArrayListSorter.mergesort(arr);
-		assertEquals(arr, ArrayListSorter.generateAscending(3));
+		assertEquals(ArrayListSorter.generateAscending(3), arr);
 	}
 	
 	@Test
 	void TestFourMergeIntegers() {
 		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(4);
 		ArrayListSorter.mergesort(arr);
-		assertEquals(arr, ArrayListSorter.generateAscending(4));
+		assertEquals(ArrayListSorter.generateAscending(4), arr);
 	}
 	
 	@Test
 	void TestFiveMergeIntegers() {
 		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(5);
 		ArrayListSorter.mergesort(arr);
-		assertEquals(arr, ArrayListSorter.generateAscending(5));
+		assertEquals(ArrayListSorter.generateAscending(5), arr);
 	}
 	
 	@Test
 	void TestManyMergeIntegers() {
 		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(100);
 		ArrayListSorter.mergesort(arr);
-		assertEquals(arr, ArrayListSorter.generateAscending(100));
+		assertEquals(ArrayListSorter.generateAscending(100), arr);
+	}
+	
+	@Test
+	void TestAlreadySortedIntegersMerge() {
+		ArrayList<Integer> arr = ArrayListSorter.generateAscending(100);
+		ArrayListSorter.mergesort(arr);
+		assertEquals(ArrayListSorter.generateAscending(100), arr);
+	}
+	
+	@Test
+	void TestDescendingIntegersMerge() {
+		ArrayList<Integer> arr = ArrayListSorter.generateDescending(10);
+		ArrayListSorter.mergesort(arr);
+		assertEquals(ArrayListSorter.generateAscending(10), arr);
 	}
 	
 	@Test
@@ -122,7 +136,7 @@ class ArrayListSorterTest {
 		expected.add("Seth");
 		ArrayListSorter.mergesort(arr);
 		
-		assertEquals(arr, expected);
+		assertEquals(expected, arr);
 	}
 	
 	@Test
@@ -137,7 +151,7 @@ class ArrayListSorterTest {
 		expected.add("Seth");
 		ArrayListSorter.mergesort(arr);
 		
-		assertEquals(arr, expected);
+		assertEquals(expected, arr);
 	}
 	
 	@Test
@@ -154,7 +168,7 @@ class ArrayListSorterTest {
 		expected.add("Seth");
 		ArrayListSorter.mergesort(arr);
 		
-		assertEquals(arr, expected);
+		assertEquals(expected, arr);
 	}
 	
 	@Test
@@ -173,7 +187,45 @@ class ArrayListSorterTest {
 		expected.add("Seth");
 		ArrayListSorter.mergesort(arr);
 		
-		assertEquals(arr, expected);
+		assertEquals(expected, arr);
+	}
+	
+	@Test
+	void TestAlreadySortedMergeStrings() {
+		ArrayList<String> arr = new ArrayList<String>();
+		ArrayList<String> expected = new ArrayList<String>();
+		arr.add("Apple");
+		arr.add("Ben");
+		arr.add("Cameron");
+		arr.add("Mike");
+		arr.add("Seth");
+		expected.add("Apple");
+		expected.add("Ben");
+		expected.add("Cameron");
+		expected.add("Mike");
+		expected.add("Seth");
+		ArrayListSorter.mergesort(arr);
+		
+		assertEquals(expected, arr);
+	}
+	
+	@Test
+	void TestDescendingMergeStrings() {
+		ArrayList<String> arr = new ArrayList<String>();
+		ArrayList<String> expected = new ArrayList<String>();
+		arr.add("Seth");
+		arr.add("Mike");
+		arr.add("Cameron");
+		arr.add("Ben");
+		arr.add("Apple");
+		expected.add("Apple");
+		expected.add("Ben");
+		expected.add("Cameron");
+		expected.add("Mike");
+		expected.add("Seth");
+		ArrayListSorter.mergesort(arr);
+		
+		assertEquals(expected, arr);
 	}
 	
 //--------------------------------Merge Sort Tests End-------------------------------------------
@@ -182,12 +234,178 @@ class ArrayListSorterTest {
 	
 //--------------------------------Quick Sort Tests Begin-----------------------------------------
 	
-//	@Test
-//	void EmptyQuick() {
-//		ArrayList<Integer> arr = new ArrayList<Integer>();
-//		ArrayListSorter.quicksort(arr);
-//		assertTrue(arr.isEmpty());
-//	}
+	@Test
+	void EmptyQuick() {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		ArrayListSorter.quicksort(arr);
+		assertTrue(arr.isEmpty());
+	}
+	
+	@Test
+	void TestOneQuickIntegers() {
+		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(1);
+		ArrayListSorter.quicksort(arr);
+		assertEquals(ArrayListSorter.generateAscending(1), arr);
+	}
+	
+	@Test
+	void TestTwoQuickIntegers() {
+		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(2);
+		ArrayListSorter.quicksort(arr);
+		assertEquals(ArrayListSorter.generateAscending(2), arr);
+	}
+	
+	@Test
+	void TestThreeQuickIntegers() {
+		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(3);
+		ArrayListSorter.quicksort(arr);
+		assertEquals(ArrayListSorter.generateAscending(3), arr);
+	}
+	
+	@Test
+	void TestFourQuickIntegers() {
+		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(4);
+		ArrayListSorter.quicksort(arr);
+		assertEquals(ArrayListSorter.generateAscending(4), arr);
+	}
+	
+	@Test
+	void TestFiveQuickIntegers() {
+		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(5);
+		ArrayListSorter.quicksort(arr);
+		assertEquals(ArrayListSorter.generateAscending(5), arr);
+	}
+	
+	@Test
+	void TestManyQuickIntegers() {
+		ArrayList<Integer> arr = ArrayListSorter.generatePermuted(100);
+		ArrayListSorter.quicksort(arr);
+		assertEquals(ArrayListSorter.generateAscending(100), arr);
+	}
+	
+	@Test
+	void TestAlreadySortedIntegersQuick() {
+		ArrayList<Integer> arr = ArrayListSorter.generateAscending(10);
+		ArrayListSorter.quicksort(arr);
+		assertEquals(ArrayListSorter.generateAscending(10), arr);
+	}
+	
+	@Test
+	void TestDescendingIntegersQuick() {
+		ArrayList<Integer> arr = ArrayListSorter.generateDescending(10);
+		ArrayListSorter.quicksort(arr);
+		assertEquals(ArrayListSorter.generateAscending(10), arr);
+	}
+	
+	@Test
+	void TestOneQuickStrings() {
+		ArrayList<String> arr = new ArrayList<String>();
+		arr.add("Mike");
+		ArrayListSorter.quicksort(arr);
+		assertEquals(arr, arr);
+	}
+	
+	@Test
+	void TestTwoQuickStrings() {
+		ArrayList<String> arr = new ArrayList<String>();
+		ArrayList<String> expected = new ArrayList<String>();
+		arr.add("Seth");
+		arr.add("Mike");
+		expected.add("Mike");
+		expected.add("Seth");
+		ArrayListSorter.quicksort(arr);
+		
+		assertEquals(expected, arr);
+	}
+	
+	@Test
+	void TestThreeQuickStrings() {
+		ArrayList<String> arr = new ArrayList<String>();
+		ArrayList<String> expected = new ArrayList<String>();
+		arr.add("Seth");
+		arr.add("Mike");
+		arr.add("Apple");
+		expected.add("Apple");
+		expected.add("Mike");
+		expected.add("Seth");
+		ArrayListSorter.quicksort(arr);
+		
+		assertEquals(expected, arr);
+	}
+	
+	@Test
+	void TestFourQuickStrings() {
+		ArrayList<String> arr = new ArrayList<String>();
+		ArrayList<String> expected = new ArrayList<String>();
+		arr.add("Seth");
+		arr.add("Mike");
+		arr.add("Apple");
+		arr.add("Ben");
+		expected.add("Apple");
+		expected.add("Ben");
+		expected.add("Mike");
+		expected.add("Seth");
+		ArrayListSorter.quicksort(arr);
+		
+		assertEquals(expected, arr);
+	}
+	
+	@Test
+	void TestFiveQuickStrings() {
+		ArrayList<String> arr = new ArrayList<String>();
+		ArrayList<String> expected = new ArrayList<String>();
+		arr.add("Seth");
+		arr.add("Mike");
+		arr.add("Apple");
+		arr.add("Ben");
+		arr.add("Cameron");
+		expected.add("Apple");
+		expected.add("Ben");
+		expected.add("Cameron");
+		expected.add("Mike");
+		expected.add("Seth");
+		ArrayListSorter.quicksort(arr);
+		
+		assertEquals(expected, arr);
+	}
+	
+	@Test
+	void TestAlreadySortedQuickStrings() {
+		ArrayList<String> arr = new ArrayList<String>();
+		ArrayList<String> expected = new ArrayList<String>();
+		arr.add("Apple");
+		arr.add("Ben");
+		arr.add("Cameron");
+		arr.add("Mike");
+		arr.add("Seth");
+		expected.add("Apple");
+		expected.add("Ben");
+		expected.add("Cameron");
+		expected.add("Mike");
+		expected.add("Seth");
+		ArrayListSorter.quicksort(arr);
+		
+		assertEquals(expected, arr);
+	}
+	
+	@Test
+	void TestDescendingQuickStrings() {
+		ArrayList<String> arr = new ArrayList<String>();
+		ArrayList<String> expected = new ArrayList<String>();
+		arr.add("Seth");
+		arr.add("Mike");
+		arr.add("Cameron");
+		arr.add("Ben");
+		arr.add("Apple");
+		expected.add("Apple");
+		expected.add("Ben");
+		expected.add("Cameron");
+		expected.add("Mike");
+		expected.add("Seth");
+		ArrayListSorter.quicksort(arr);
+		
+		assertEquals(expected, arr);
+	}
 	
 	
 //--------------------------------Quick Sort Tests End-----------------------------------------
